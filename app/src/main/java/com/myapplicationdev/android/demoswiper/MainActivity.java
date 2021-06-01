5997bbee285e1a37fc09106c778496ac4d56b7bb
Daryl <darylgialolo@gmail.com>
2021-06-01 10:44:06 +0800

    2nd Commit - Section C Completed

--- app/src/main/java/com/myapplicationdev/android/demoswiper/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoswiper/MainActivity.java
@@ -6,14 +6,21 @@ import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    ArrayList<Fragment> fragments;
-    MyFragmentPagerAdapter fragmentPagerAdapter;
-    ViewPager viewPager;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ArrayList<Fragment> fragments;
+    private MyFragmentPagerAdapter fragmentPagerAdapter;
+    private ViewPager viewPager;
+
+    private Button backButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = findViewById(R.id.view_pager_1);
+        backButton = findViewById(R.id.back_button);
+        nextButton = findViewById(R.id.next_button);
+        backButton.setOnClickListener(this::onClick);
+        nextButton.setOnClickListener(this::onClick);
 
         // Add Fragments
         fragments = new ArrayList<>();
@@ -33,5 +44,30 @@ public class MainActivity extends AppCompatActivity {
 
         // Set View Pager's Adapter
         viewPager.setAdapter(fragmentPagerAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back_button:
+                // Go to Previous Fragment - if previous exists
+                Log.d(TAG, "Back Button Pressed");
+                Log.d(TAG, "Item: " + viewPager.getCurrentItem());
+                if (viewPager.getCurrentItem() > 0) {
+                    int previousPage = viewPager.getCurrentItem() - 1;
+                    viewPager.setCurrentItem(previousPage, true);
+                }
+                break;
+            case R.id.next_button:
+                // Go to Next Fragment - if next exists
+                Log.d(TAG, "Next Button Pressed");
+                Log.d(TAG, "Item: " + viewPager.getCurrentItem());
+                int max = viewPager.getChildCount();
+                if (viewPager.getCurrentItem() < max - 1) {
+                    int nextPage = viewPager.getCurrentItem() + 1;
+                    viewPager.setCurrentItem(nextPage, true);
+                }
+
+        }
     }
 }
